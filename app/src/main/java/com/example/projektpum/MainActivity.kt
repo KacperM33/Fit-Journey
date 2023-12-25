620f292e38a7a782a81afbfce8cbbb4e18d85328
KacperM33 <kacper.mroczek33@gmail.com>
2023-12-25 19:57:00 +0100

    Commit2

--- app/src/main/java/com/example/projektpum/MainActivity.kt
+++ app/src/main/java/com/example/projektpum/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.projektpum
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,35 +14,29 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.projektpum.ui.theme.ProjektPUMTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ProjektPUMTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
+        setContentView(R.layout.layout)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjektPUMTheme {
-        Greeting("Android")
+//        findViewById<Button>(R.id.button).setOnClickListener {
+//            findViewById<TextView>(R.id.textView).setText("Cześć!")
+//            var message = Toast.makeText(applicationContext, "Cześć!", Toast.LENGTH_SHORT)
+//            message.show()
+//        }
+//        findViewById<Button>(R.id.button2).setOnClickListener {
+//            findViewById<TextView>(R.id.textView).setText("Dzień dobry!")
+//        }
+//
+//        findViewById<Button>(R.id.buttonYT).setOnClickListener {
+//            val address = "https://www.youtube.com/watch?v=uiEz0_kMHaY&t"
+//            val intentYT = Intent(Intent.ACTION_VIEW, Uri.parse(address))
+//            startActivity(intentYT)
+//        }
     }
 }
