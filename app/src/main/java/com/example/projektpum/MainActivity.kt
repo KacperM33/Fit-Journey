29f98242131904754b8861b6604d5309c4ebc583
KacperM33 <kacper.mroczek33@gmail.com>
2023-12-27 16:30:34 +0100

    Camera

--- app/src/main/java/com/example/projektpum/MainActivity.kt
+++ app/src/main/java/com/example/projektpum/MainActivity.kt
@@ -1,42 +1,95 @@
 package com.example.projektpum
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
-import android.net.Uri
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.projektpum.ui.theme.ProjektPUMTheme
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.Date
+import java.util.Locale
+
 
 class MainActivity : ComponentActivity() {
+    private val camera_permission = 100
+    private val storage_permission = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.layout)
 
-//        findViewById<Button>(R.id.button).setOnClickListener {
-//            findViewById<TextView>(R.id.textView).setText("Cześć!")
-//            var message = Toast.makeText(applicationContext, "Cześć!", Toast.LENGTH_SHORT)
-//            message.show()
-//        }
-//        findViewById<Button>(R.id.button2).setOnClickListener {
-//            findViewById<TextView>(R.id.textView).setText("Dzień dobry!")
-//        }
-//
-//        findViewById<Button>(R.id.buttonYT).setOnClickListener {
-//            val address = "https://www.youtube.com/watch?v=uiEz0_kMHaY&t"
-//            val intentYT = Intent(Intent.ACTION_VIEW, Uri.parse(address))
-//            startActivity(intentYT)
-//        }
+        findViewById<Button>(R.id.camera_button).setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), camera_permission)
+            }
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), storage_permission)
+            }
+
+            val intentCapture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intentCapture, 123)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == camera_permission) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                Toast.makeText(this, "Uprawnienia do kamery są wymagane.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        if (requestCode == storage_permission) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                Toast.makeText(this, "Uprawnienia do folderu są wymagane.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null && requestCode == 123) {
+            val bmp = data.extras?.get("data") as Bitmap
+
+            bmp?.let {
+                saveImageToMediaStore(it)
+            }
+        }
+    }
+
+    private fun saveImageToMediaStore(imageBitmap: Bitmap) {
+        val displayName = "IMG_${SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())}.jpg"
+        val mimeType = "image/jpeg"
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME, displayName)
+            put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+        }
+
+        val resolver = contentResolver
+        val uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        uri?.let {
+            resolver.openOutputStream(it)?.use { outputStream ->
+                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                outputStream.flush()
+            }
+        }
     }
 }
